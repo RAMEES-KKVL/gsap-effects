4114c0e4c318e2bac81c255e5a7cb36d04b04698
RAMEES-KKVL <rameesp41750@gmail.com>
2024-10-16 01:33:55 +0530

    Implemented parallax background effect

---
+++ src/app/parallax-bg/parallax-bg.component.ts
@@ -0,0 +1,37 @@
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+@Component({
+  selector: 'app-parallax-bg',
+  templateUrl: './parallax-bg.component.html',
+  styleUrls: ['./parallax-bg.component.css']
+})
+export class ParallaxBgComponent implements OnInit, AfterViewInit {
+  @ViewChildren("bgClass", { read: ElementRef }) bgSection!: QueryList<ElementRef>
+  
+  ngOnInit(): void {
+  } 
+  
+  ngAfterViewInit(): void {
+    this.bgSection.forEach((section: ElementRef, i: number) => {
+      const element = section.nativeElement;
+      const bgImg = element.querySelector(`.bg-img_${i + 1}`);
+      
+      const getRatio = (el: HTMLElement) => window.innerHeight / (window.innerHeight + el.offsetHeight);
+      
+      gsap.fromTo(bgImg, {
+        backgroundPosition: `50% ${-window.innerHeight * getRatio(element)}px`,
+      }, {
+        backgroundPosition: `50% ${window.innerHeight * (1 - getRatio(element))}px`,
+        ease: 'none',
+        scrollTrigger: {
+          trigger: element,
+          scrub: true,
+          invalidateOnRefresh: true
+        }
+      });
+    });
+  }
+}
